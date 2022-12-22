906fff8456f871a346e54c3fcc8dee12bd811628
Vitaly <shishov_84@mail.ru>
2022-12-22 13:50:34 +0300

    change css module

--- src/components/BestOffer/BestOffer.js
+++ src/components/BestOffer/BestOffer.js
@@ -1,31 +1,31 @@
 import React from "react";
-import card from './BestOffer.module.css';
+import bestoffer from './BestOffer.module.css';
 import bitcoin from './bitcoin.svg';
 import arrow from './Arrow.svg';
 
 function BestOffer() {
   return (
-      <section className={card.cards_wrapper}>
-          <div className={card.card}>
-              <div className={card.ellipse}></div>
+      <section className={bestoffer.cards_wrapper}>
+          <div className={bestoffer.card}>
+              <div className={bestoffer.ellipse}></div>
               <h2>Выгодное предложение</h2>
-              <div className={card.text}>
+              <div className={bestoffer.text}>
                   <div>-20%</div>
                   <div>на все товары Antminer</div>
               </div>
-              <div className={card.image_product}><img src={bitcoin} alt='foto_bitcoin'/></div>
-              <div className={card.button}>
+              <div className={bestoffer.image_product}><img src={bitcoin} alt='foto_bitcoin'/></div>
+              <div className={bestoffer.button}>
                   <a href='#'>
                       <div>
                           В каталог
                       </div>
                       <div>
-                          <img src={arrow}/>
+                          <img src={arrow} alt='foto_arrow'/>
                       </div>
                   </a>
               </div>
-              <div className={card.ellipse_bottom}></div>
-              <div className={card.ellipse_top}></div>
+              <div className={bestoffer.ellipse_bottom}></div>
+              <div className={bestoffer.ellipse_top}></div>
           </div>
 
       </section>
